d8fae9e4a19139911efe365c9a13c7f20c694783
felep1337 <cyhka312@mail.ru>
2025-10-19 19:41:46 +0800

    чет начал 2

--- Num_Met_1/Num_Met_1.cpp
+++ Num_Met_1/Num_Met_1.cpp
@@ -54,7 +54,7 @@ double func(float x) {
     return sin(x) * log(x+1);
 }
 
-double interp(float x, int n, double* numbers, double* arr) {
+double LangrangePolynomial(float x, int n, double* numbers, double* arr) {
     double answer = 0;
     for (int i = 0; i <= n; i++) {
         double result = 1.0;
@@ -70,11 +70,20 @@ double interp(float x, int n, double* numbers, double* arr) {
     return answer;
 }
 
+double* Chebyshev(int n) {
+    double* arr = new double[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = (b + a) / 2 + (b - a) / 2 * cos(M_PI * (2 * i + 1) / 2 * n);
+
+    }
+    return arr;
+}
+
 double Supremum(int k, double* numbers, double* array) {
     double supremum = 0;
     for (int i = 0; i <= nodes; i++) {
         double current = ((i * b) / nodes);
-        double current_val = (func(current) - interp(current, k, numbers, array));
+        double current_val = (func(current) - LangrangePolynomial(current, k, numbers, array));
         if (current_val > supremum) {
             supremum = current_val;
         }
@@ -82,18 +91,58 @@ double Supremum(int k, double* numbers, double* array) {
     return supremum;
 }
 
-void makeDataForError(std::ofstream *file, int k, double* numbers, double* array) {
+void makeDataErrorFile(int k, double* numbers, double* array) {
+    std::ofstream file_err("data_err.txt");
     for (int i = 0; i <= nodesForError; i++) {
         double current = ((i * b) / nodesForError);
-        *file << current << " " << func(current) - interp(current, k, numbers, array) << "\n";
+        file_err << current << " " << func(current) - LangrangePolynomial(current, k, numbers, array) << "\n";
+    }
+    file_err.close();
+}
+
+void makeDataEvenly(int n, double* numbers, double* array) {
+    for (int i = 0; i <= n; i++) {
+        array[i] = func(b * i / n);
+        numbers[i] = (b * i / n);
+    }
+}
+void makeDataUnevenly(int n, double* numbers, double* array) {
+    for (int i = 0; i < n; i++) {
+        array[i] = func(numbers[i]);
     }
 }
 
-void makeData(int k, double* numbers, double* array) {
-    for (int i = 0; i <= k; i++) {
-        array[i] = func(b * i / k);
-        numbers[i] = (b * i / k);
+int processData1(int n) {
+    std::ofstream file("data.txt");
+    int best = 1;
+    double temp = 1000;
+    for (int k = 1; k <= n; k++) {
+        double* array = new double[k + 1];
+        double* numbers = new double[k + 1];
+
+        makeDataEvenly(k, numbers, array);
+        double supremum = Supremum(k, numbers, array);
+        if (temp > supremum){
+            best = k;
+            temp = supremum;
+        }
+        file << k << " " << supremum << "\n";
+        if (k == n) {
+            makeDataErrorFile(k, numbers, array);
+        }
+
+        delete[] array;
+        delete[] numbers;
     }
+    file.close();
+    return best;
+}
+
+void processData2(int best) {
+    double* arr = Chebyshev(best);
+    double* arr_val = new double[best];
+    makeDataUnevenly(best, arr, arr_val);
+
 }
 
 int main()
@@ -103,25 +152,8 @@ int main()
     std::cout << "Введите кол-во узлов: ";
     std::cin >> n;
 
-    std::ofstream file("data.txt");
-
-    for (int k = 1; k <= n; k++) {
-        double* array = new double[k + 1];
-        double* numbers = new double[k + 1];
-
-        makeData(k,numbers,array);
-        double supremum = Supremum(k, numbers, array);
-        file << k << " " << supremum << "\n";
-        if (k == n) {
-            std::ofstream file_err("data_err.txt");
-            makeDataForError(&file_err,k, numbers, array);
-            file_err.close();
-        }
-
-        delete[] array;
-        delete[] numbers;
-    }
-    file.close();
+    int best = processData1(n);
+    processData2(best);
 
     int choose = -1;
     while (choose != 0){
